a0548634679be69555b105c8c88b01f69dccfeb3
Carlos Schramer <carlos.schramer@sealsystems.de>
2022-03-22 17:38:02 +0100

    Change run options.

--- create.sh
+++ create.sh
@@ -7,7 +7,7 @@ echo ""
 echo "Creating and running the Jenkins Docker container \"jenkins\"..."
 mkdir -p /var/jenkins
 chown -R 1000:1000 /var/jenkins
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name jenkins -d jenkins-docker
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --detach --name jenkins jenkins-docker
 echo "Done."
 
 echo ""
